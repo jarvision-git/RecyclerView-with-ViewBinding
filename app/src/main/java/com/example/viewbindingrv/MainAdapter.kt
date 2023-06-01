7a991189e869357623d7fd936eb08a1c72280b25
Kushal <kushalldesai@gmail.com>
2023-06-01 13:35:10 +0530

    final commit. RecyclerView with ViewBinding

---
+++ app/src/main/java/com/example/viewbindingrv/MainAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.viewbindingrv
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.viewbindingrv.databinding.RecyclerviewItemBinding
+
+class MainAdapter(val taskList: List<Task>):RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+    inner class MainViewHolder(val itemBinding:RecyclerviewItemBinding):
+        RecyclerView.ViewHolder(itemBinding.root)
+    {
+            fun binditem(task:Task){
+                itemBinding.titleTv.text=task.title
+                itemBinding.timeTv.text=task.timestamp
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        return MainViewHolder(RecyclerviewItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return taskList.size
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        val task=taskList[position]
+        holder.binditem(task)
+    }
+
+}
---
+++ app/src/main/java/com/example/viewbindingrv/TaskList.kt
@@ -0,0 +1,16 @@
+package com.example.viewbindingrv
+
+object TaskList {
+    val taskList=listOf<Task>(
+        Task("Take a Walk", timestamp = "7:00 am"),
+        Task("Eat BreakFast", timestamp = "8:00 am"),
+        Task("Attend a Meeting", timestamp = "9:00 am"),
+        Task("Read a Book", timestamp = "12:00 am"),
+        Task("Eat Lunch", timestamp = "2:00 pm"),
+        Task("Buy Groceries", timestamp = "4:00 pm"),
+        Task("Watch Youtube Videos", timestamp = "6:00 pm"),
+        Task("Eat Dinner", timestamp = "7:00 pm"),
+        Task("Go to Bed", timestamp = "9:00 pm")
+
+    )
+}
